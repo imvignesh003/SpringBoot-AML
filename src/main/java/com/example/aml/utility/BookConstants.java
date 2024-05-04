0d300dfb236adbc3d5cb04beee41d7a7a07d63ae
Adam Abdulhamid <adam13132002@hotmail.com>
2024-05-03 22:17:50 -0400

    Fix code smells 2

--- src/main/java/com/example/aml/utility/BookConstants.java
+++ src/main/java/com/example/aml/utility/BookConstants.java
@@ -1,5 +1,9 @@
 package com.example.aml.utility;
 
-public class BookConstants {
+public final class BookConstants {
+    private BookConstants() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
     public static final String MISSING_IMAGE_ID = "018f4078-a5d6-75b7-8848-bfdc3a7775e4";
 }
