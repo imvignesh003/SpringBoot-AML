f6f48ff0cc73017a05d331b700e79f5dd7af1243
Adam Abdulhamid <adam13132002@hotmail.com>
2024-05-03 22:15:13 -0400

    Fix code smells

--- src/main/java/com/example/aml/utility/BookConstants.java
+++ src/main/java/com/example/aml/utility/BookConstants.java
@@ -1,8 +1,5 @@
 package com.example.aml.utility;
 
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
 public class BookConstants {
-    public static String MISSING_IMAGE_ID = "018f4078-a5d6-75b7-8848-bfdc3a7775e4";
+    public static final String MISSING_IMAGE_ID = "018f4078-a5d6-75b7-8848-bfdc3a7775e4";
 }
